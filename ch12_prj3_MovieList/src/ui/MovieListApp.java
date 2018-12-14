43b0c4d55a1a1ca4075dbf95acf6a99edacb72d3
Gary Henderson <hendergy@gmail.com>
2018-12-13 22:56:55 -0500

    initial commit

---
+++ ch12_prj3_MovieList/src/ui/MovieListApp.java
@@ -0,0 +1,43 @@
+package ui;
+
+import java.util.ArrayList;
+
+import business.Movie;
+import db.MovieIO;
+import util.Console;
+
+public class MovieListApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to the Movie List App\n");
+		
+		Console console = new Console();
+		MovieIO mIO = new MovieIO();
+		
+		String choice = "y";
+		
+		// tell user how many movies are available
+		
+		while(choice.equalsIgnoreCase("y")) {
+			System.out.println("Choose from " + mIO.getMovies().size() + " movies.");
+			System.out.println("Categories: drama | musical | scifi | horror | comedy | animated");
+		
+			
+			String category = console.getString("Enter a category: ");
+			
+			ArrayList<Movie> filteredMovies = mIO.getMovies(category);
+			
+			for(Movie m : filteredMovies) {
+				System.out.println(m.getTitle());
+			}
+			
+		// continue
+		choice = console.getString("Continue? (y/n) ");
+		}
+		// goodbye
+		System.out.println("Thank you for using the Movie List App\nGoodbye!");
+	}
+
+}
